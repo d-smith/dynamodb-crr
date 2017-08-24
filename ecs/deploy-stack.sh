56a1f8df4cfa4e7d511f950b05ee17ae5bfadef4
d-smith <doug.smith.mail@gmail.com>
2017-08-24 12:09:28 +0100

    Added stack to run replicator in an ecs cluster

--- ecs/deploy-stack.sh
+++ ecs/deploy-stack.sh
@@ -2,9 +2,9 @@
 
 aws cloudformation create-stack --stack-name replicator \
 --template-body file://replicator.yml --parameters \
-ParameterKey=BucketRoot,ParameterValue=https://s3.amazonaws.com/admin-doug-deploy-us-east-1 \
+ParameterKey=BucketRoot,ParameterValue=$BUCKET_ROOT \
 ParameterKey=KeyName,ParameterValue=FidoKeyPair \
 ParameterKey=SourceTable,ParameterValue=serverless-rest-api-with-dynamodb-hc1 \
 ParameterKey=DestTable,ParameterValue=serverless-rest-api-with-dynamodb-hc1 \
 ParameterKey=ClusterName,ParameterValue=CharlieFoxtrot \
---capabilities CAPABILITY_IAM
+--capabilities CAPABILITY_IAM --on-failure DO_NOTHING
--- ecs/stage-files.sh
+++ ecs/stage-files.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export DEPLOY_BUCKET=admin-doug-deploy-us-east-1
-
 for f in *.y*ml; do aws s3 cp $f s3://$DEPLOY_BUCKET; done
